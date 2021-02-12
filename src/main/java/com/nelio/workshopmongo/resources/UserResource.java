b385f4d18af95b1bedb727c2282b0e476080b808
ramilani12 <ramilani@gmail.com>
2021-02-12 16:45:15 -0300

    Serviço Inserção de Usuário

--- src/main/java/com/nelio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelio/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.nelio.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelio.workshopmongo.domain.User;
 import com.nelio.workshopmongo.dto.UserDTO;
@@ -37,5 +41,19 @@ public class UserResource {
 		
 		return ResponseEntity.ok().body(new UserDTO(user));
 	}
+	
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody final UserDTO dto) {
+		
+		User user = userService.fromDTO(dto);
+		
+		user = userService.insert(user);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
+	}	
 
 }
--- src/main/java/com/nelio/workshopmongo/services/UserService.java
+++ src/main/java/com/nelio/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelio.workshopmongo.domain.User;
+import com.nelio.workshopmongo.dto.UserDTO;
 import com.nelio.workshopmongo.repository.UserRepository;
 import com.nelio.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,14 @@ public class UserService {
 	public User findById(final String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId() , objDTO.getName() , objDTO.getEmail());
 	}
 }
