3dea0bb58fa83663544889b7755e29e4e678148b
ramilani12 <ramilani@gmail.com>
2021-04-18 16:48:41 -0300

    Query Methods Titulo

--- src/main/java/com/nelio/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelio/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.nelio.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import com.nelio.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository  extends MongoRepository<Post, String>{
+	
+	public List<Post> findByTitleContainingIgnoreCase(String title);
 	
 	
 }
--- src/main/java/com/nelio/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nelio/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.nelio.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelio.workshopmongo.domain.Post;
+import com.nelio.workshopmongo.resources.util.URL;
 import com.nelio.workshopmongo.services.PostService;
 
 @RestController
@@ -23,6 +27,17 @@ public class PostResource {
 		Post post = postService.findById(id);
 		
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text" , defaultValue = "") String text) {
+		
+		text = URL.decodeParam(text);
+		
+		List<Post> posts = postService.findByTitle(text);
+		
+		
+		return ResponseEntity.ok().body(posts);
 	}
 	
 
